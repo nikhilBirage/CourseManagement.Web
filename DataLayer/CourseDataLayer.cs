15d67cddb21981225a6f54510d843dcf9954ef17
Nikhil <biragenikhil@gmail.com>
2021-12-30 20:48:15 +0530

    Add project files.

---
+++ DataLayer/CourseDataLayer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseManagement.Web.DataLayer
+{
+    public class CourseDataLayer
+    {
+
+    }
+}
